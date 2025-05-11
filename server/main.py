356f2c7029db3f7bfa9f76f0035961fad9e92518
nguoingoaihanhtinh <22520688@gm.uit.edu.vn>
2025-05-11 21:32:18 +0700

    small fix

--- server/main.py
+++ server/main.py
@@ -1,24 +1,14 @@
-import os
+import pandas as pd
+import numpy as np
 
-import redis
-from dotenv import load_dotenv
-
-load_dotenv()
-
-host = os.getenv('REDIS_HOST')
-port = os.getenv('REDIS_PORT')
-pw= os.getenv('REDIS_PASSWORD')
-ssl= os.getenv('REDIS_SSL')
-
-r = redis.Redis(
-  host=host,
-  port=port,
-  password=pw,
-  ssl=ssl
-)
-
-# r.set('foo', 'bar')
-print(r.get('foo'))
-    
+# Tạo DataFrame với 10 hàng và 5 cột, một số ô chứa giá trị NaN
+financial_data = pd.DataFrame({
+    'Revenue':    [1000, 1500, np.nan, 1700, 2000, 1800, np.nan, 1600, 2100, 1900],
+    'Expenses':   [800, 950, 1000, np.nan, 1100, 1050, 990, np.nan, 1150, 1080],
+    'Profit':     [200, 550, np.nan, 700, 900, 750, np.nan, 600, 950, 820],
+    'Assets':     [5000, np.nan, 5100, 5300, np.nan, 5500, 5400, 5200, np.nan, 5600],
+    'Liabilities':[3000, 3200, np.nan, 3100, 3300, np.nan, 3400, 3150, 3350, np.nan]
+})
 
 
+financial_data.to_csv('', index=False)
--- server/run.py
+++ server/run.py
@@ -2,5 +2,9 @@ from app import create_app
 
 app = create_app()
 
+# Liệt kê routes
+for rule in app.url_map.iter_rules():
+    print(f"{rule.endpoint:30s} {rule.methods} {rule}")
+
 if __name__ == '__main__':
     app.run(debug=True)
